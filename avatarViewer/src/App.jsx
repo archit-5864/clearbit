4ef0b558b19726d49fd8a7da6463795e02ece7c7
Archit Sharma <architsharma05072000.com>
2024-01-24 12:57:25 +0530

    Second commit

--- avatarViewer/src/App.jsx
+++ avatarViewer/src/App.jsx
@@ -1,12 +1,54 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Autosuggest from 'react-autosuggest';
 
 function App() {
   const [input, setInput] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
   const [type, setType] = useState('');
   const [image, setImage] = useState('');
 
+  const getSuggestions = async (value) => {
+    try {
+      const response = await axios.post('http://localhost:3000/autocomplete', {
+        input: value,
+      });
+      setSuggestions(response.data.suggestions);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  const onSuggestionsFetchRequested = ({ value }) => {
+    getSuggestions(value);
+  };
+
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const onSuggestionSelected = async (_, { suggestionValue }) => {
+    const domainMatch = suggestionValue.match(/\((.*?)\)/);
+    const domain = domainMatch ? domainMatch[1] : '';
+    setInput(`https://${domain}`);
+  };
+
+  const renderSuggestion = (suggestion) => <div>{suggestion}</div>;
+
+  const inputProps = {
+    placeholder: 'Enter Company Name',
+    value: input,
+    onChange: (e, { newValue }) => setInput(newValue),
+  };
+
   const handleSubmit = async () => {
+    if (input.trim() === '') {
+      alert('Please enter a valid company name.');
+      return;
+    }
+
+    // Alert to confirm before proceeding
+    // const confirmAlert = window.confirm('Proceed with fetching the avatar?');
     try {
       const response = await axios.post('http://localhost:3000/getAvatar', {
         input: input,
@@ -15,22 +57,24 @@ function App() {
       setType(type);
       setImage(image);
     } catch (error) {
-      alert("Enter Valid String ")
+      alert('Error fetching avatar. Please try again.');
       console.error('Error:', error.message);
     }
+
   };
 
   return (
     <div>
       <h1>Avatar Viewer</h1>
-      <label>
-        Enter Email or Website:
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-      </label>
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={onSuggestionsClearRequested}
+        onSuggestionSelected={onSuggestionSelected}
+        getSuggestionValue={(suggestion) => suggestion}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
       <button onClick={handleSubmit}>Get Avatar</button>
 
       {type && image && (
